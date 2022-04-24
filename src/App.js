64554c26f64125176ca7ec1e6ffd181fe1a7ba6d
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-04-24 13:40:58 +0100

    code: initialize boilerplate

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>Widget app in here</div>
+    )
+  }
+}
+
+export default App;
