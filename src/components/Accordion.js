8f2e5a283ec7681f2abf108c901e4635b14315e2
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-04-26 10:16:55 +0100

    code: add state to the functional component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,15 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Accordion = ({items}) => {
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const itemClick = (index) => {
+        setActiveIndex(index);
+    }
+
     const renderedList = items.map((item, i) =>{
         return <Fragment key={`accordion ${i}`}>
-                    <div className='title active'>
+                    <div className='title active' onClick={() => itemClick(i)}>
                         {item.title}
                         <i className='dropdown icon'></i>
                     </div>
@@ -13,7 +19,14 @@ const Accordion = ({items}) => {
                 </Fragment>
     });
 
-  return <div className='ui styled accordion'>{renderedList}</div>
+  return (
+    <Fragment>
+         <div className='ui styled accordion'>
+            {renderedList}
+        </div>
+        <h1>Active Accordion is the accordion-{activeIndex}</h1>
+    </Fragment>
+  );
 
 }
 
