a5321d685c842a918e9d9e54054a58d1cf7e3c0c
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-04-26 23:29:55 +0100

    code: link search result to wikipedia page

--- src/components/Search.js
+++ src/components/Search.js
@@ -28,9 +28,18 @@ const Search = () => {
         const regex = /(<([^>]+)>)/gi;  //NEW
         const cleanSnippet = result.snippet.replace(regex, ""); //NEW 
         // {result.snippet} was replaced with {cleanSnippet}
-        
+
         return(
             <div key={result.pageid} className='item'>
+                <div className='right floated content'>
+                    <a 
+                       className='ui button'
+                       href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                       target="_blank"
+                    >
+                        Go
+                    </a>
+                </div>
                 <div className='content' style={{ paddingTop: '15px', paddingBottom: '15px'}}>
                     <div className='header' style={{ marginBottom: '8px'}}> {result.title}</div> 
                     {/* <span dangerouslySetInnerHTML={{__html:result.snippet}}></span> */}
