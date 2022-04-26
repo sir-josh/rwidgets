5eb3e7bed9c30fdab6640cbdc44f86b2ce27c75f
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-04-26 16:06:12 +0100

    code: add text input with hooks

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Search = () => {
-  return (
-    <div>Search</div>
-  )
+    const [term, setTerm] = useState('');
+
+    return (
+        <div className='ui form'>
+            <div className='field'>
+                <label>Enter search term</label>
+                <input 
+                    type='text' 
+                    className='input-text' 
+                    value={term}
+                    onChange={e => setTerm(e.target.value)}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default Search;
