53996cca95f0cb5500fd82d31e3ee29c3b267707
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-04-26 19:24:56 +0100

    code: execute api request from useEffect using default search term

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [results, setResults] = useState([]);
+
+    console.log(results);
+
+    useEffect(() => {
+        const searchWiki = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+                }
+            });
+
+            setResults(data.query.search);
+        }
+
+        searchWiki();
+    }, [term]);
 
     return (
         <div className='ui form'>
