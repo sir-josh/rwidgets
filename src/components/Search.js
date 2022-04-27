efb07278990f1b125e41c3adb2d14e9d5f47167e
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-04-27 11:30:55 +0100

    code: throttling API request using setTimeOut, implementing a delayed request with useEffect cleanup function

--- src/components/Search.js
+++ src/components/Search.js
@@ -21,10 +21,16 @@ const Search = () => {
             setResults(data.query.search);
         }
 
-        if (term) {
-            searchWiki();
+        const timeoutId = setTimeout(() => {
+            if (term) {
+                searchWiki();
+            }
+        }, 500);
+
+        return () => {
+            clearTimeout(timeoutId);
         }
-        
+
     }, [term]);
 
     const renderedResult = results.map(result => {
