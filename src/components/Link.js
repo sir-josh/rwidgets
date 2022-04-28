760585b826c05c568519a5fe58240ca5b570f234
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-04-28 12:55:24 +0100

    code: detecting navigation changes

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const Link = ({path, className, children}) => {
-  return (
-    <a href={path} className={className}>{children}</a >
-  )
+const Link = ({href, className, children}) => {
+    const onNavClick = event => {
+        event.preventDefault();
+        window.history.pushState({}, '', href);
+
+        //Setup an event to tell each navigation link (on Navigation component) to listen for when the URL changes
+        const navEvent = new PopStateEvent('popstate');
+        window.dispatchEvent(navEvent);
+    }
+
+    return (
+        <a onClick={onNavClick} href={href} className={className}>{children}</a >
+    );
 }
 
 export default Link;
