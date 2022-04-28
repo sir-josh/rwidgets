3014cbda1d495803c8582124193841041baa9d9b
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-04-28 13:28:11 +0100

    code: handle control button clicks on link tags

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,6 +2,13 @@ import React, { useEffect } from 'react';
 
 const Link = ({href, className, children}) => {
     const onNavClick = event => {
+        // if a user makes use of control or command key to click the key, then use normal browser default 
+        // behavior, that is to open in a new tab
+        
+        if(event.metaKey || event.ctrlKey){
+            return;
+        }
+
         event.preventDefault();
         window.history.pushState({}, '', href);
 
