8d219a0d436dc7b6b621401dcf5e3ae81b12a7c2
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-04-28 01:00:26 +0100

    code: display translated text

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,21 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 const APIKEY = 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM';
 
 const Convert = ({language, text}) => {
+    const [translated, setTranslated] = useState('');
+
     useEffect(() => {
-        axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
-            params:{
-                q: text,
-                target: language.value,
-                key: APIKEY
-            }
-        });
+        const doTranslation = async () => {
+            const { data:{ data }} = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+                params:{
+                    q: text,
+                    target: language.value,
+                    key: APIKEY
+                }
+            });
+
+            setTranslated(data.translations[0].translatedText);
+        }
+        
+        doTranslation();
+
     }, [language, text]);
 
-  return (
-    <div>Convert Text Here</div>
-  );
+    return (
+        <div>
+            <h3>{translated}</h3>
+        </div>
+    );
 }
 
 export default Convert;
